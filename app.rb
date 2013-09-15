5af93cbc5cb0e980bdf83323f01b89ab4d3ae75b
Richard Enlow <richard@amoe.ba>
2013-09-14 17:05:41 -0700

    Slight cleanup to ready for staging

--- app.rb
+++ app.rb
@@ -1,14 +1,6 @@
 require './lib/models.rb'
 require './lib/helpers.rb'
 
-#set :views, , :default => "views"
-
-#get '/stylesheets/*.css' do
-#  filename = params[:splat].first
-#  scss "#{filename}.css".to_sym, :views => 'lib/assets/stylesheets'
-#  #scss :stylesheet, :style => :expanded
-#end
-
 get '/stylesheets/*.css' do
   content_type 'text/css', :charset => 'utf-8'
   filename = params[:splat].first
@@ -19,17 +11,6 @@ end
 get '/styles.css' do
 	  scss :styles
 end
-
-
-#get '/stylesheets/:name.css' do
-#  content_type 'text/css', :charset => 'utf-8'
-#
-
-#get "/stylesheets/:stylesheet.css" do |stylesheet|
-#  content_type "text/css"
-#  template = File.read(File.join('lib/assets/stylesheets', "#{stylesheet}.css.scss"))
-#  Sass::Engine.new(template).render
-#end
 
 # Set up root path and use the index view
 get '/' do
